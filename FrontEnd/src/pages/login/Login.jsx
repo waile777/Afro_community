9bdf9395f5cecb07332a517652d9624be31e1403
= <=>
2026-03-28 12:24:31 +0100

    add check infos (waiting) in database

--- FrontEnd/src/pages/login/Login.jsx
+++ FrontEnd/src/pages/login/Login.jsx
@@ -9,17 +9,17 @@ function Login() {
     const navigate = useNavigate()
     const [form, setForm] = useState({
         email: '',
-        password: ''
+        password: '',
+        statePage : 'idle' // idle | checking | error
     })
     const [errors, setErrors] = useState({
-        emailEmpty: '',
-        passwordEmpty: '',
+        emailError: '',
+        passwordError: '',
     })
     const [passwordOpen, setPasswordOpen] = useState(false)
     const handleChangeForm = (e) => {
         const name = e.target.name
         const value = e.target.value
-        console.log(value);
 
         setForm((form) => {
             return {
@@ -30,35 +30,53 @@ function Login() {
     }
     useEffect(() => {
         console.log(errors);
-
     }, [errors])
 
-
+    const handleError = (err) => {
+    if (err.response) {
+        console.log(err.response.status);
+        
+        switch (err.response.status) {
+        case 401:
+            return setErrors((err) => {
+                return {
+                    ...err,
+                    passwordError: 'The email address or password you entered isn\'t connected to an account.'
+                }
+            }
+            )
+        case 500:
+            return setErrors((err) => {
+                return {
+                    ...err,
+                    passwordError: 'Server error, please try again later'
+                }
+            }
+            )
+        default:
+            return setErrors((err) => {
+                return {
+                    ...err,
+                    passwordError: 'Something went wrong'
+                }
+            }
+            )
+        }
+    }
+    return 'Check your internet connection';
+    };
     const handleSubmit = async (e) => {
         e.preventDefault()
         setErrors({
-            emailEmpty: '',
-            passwordEmpty: '',
+            emailError: '',
+            passwordError: '',
         })
-        if (form.email === "") {
-            setErrors((err) => {
-                return {
-                    ...err,
-                    emailEmpty: 'Email Must Be Not empty'
-                }
-
+        setForm((prev) => {
+            return {
+                ...prev,
+                statePage : 'waiting'
             }
-            )
-        }
-        if (form.password === "") {
-            setErrors((err) => {
-                return {
-                    ...err,
-                    passwordEmpty: 'Password Must Be Not empty'
-                }
-            }
-            )
-        }
+        })
         try {
             const res = await api.post('/login', form)
 
@@ -67,7 +85,13 @@ function Login() {
             console.log(res.data)
             navigate("/discover")
         } catch (err) {
-            console.log(err.response?.data || err.message)
+            handleError(err)
+            setForm((prev) => {
+            return {
+                ...prev,
+                statePage : 'error'
+            }
+        })
         }
     }
     return (
@@ -79,15 +103,16 @@ function Login() {
                         <label htmlFor="email">Email</label>
                         <input name="email" type="text" onChange={(e) => handleChangeForm(e)} value={form.email} />
                     </div>
-                    {errors.emailEmpty !== "" && <p className="error">{errors.emailEmpty}</p>}
+                    {errors.emailError !== "" && <p className="error">{errors.emailError}</p>}
                     <div className="container-input password">
                         <label htmlFor="first-name">Password</label>
                         <input type={passwordOpen ? 'text' : 'password'} onChange={(e) => handleChangeForm(e)} value={form.password} name='password' />
                         <img className='password-eye' src={!passwordOpen ? closeEye : openEye} alt="eye" onClick={() => setPasswordOpen(!passwordOpen)} />
                     </div>
-                    {errors.passwordEmpty !== "" && <p className="error">{errors.passwordEmpty}</p>}
+                    {errors.passwordError !== "" && <p className="error">{errors.passwordError}</p>}
                     <div className="button-section">
-                        <button type="submit">Log In</button>
+                        <div className={`waiting-icon ${form.statePage === "waiting" ? "waiting" : undefined}`}></div>
+                        <button type="submit" className={`${form.statePage === "waiting" ? "waiting" : undefined}`}>Log In</button>
                         <div className="bottom-button-section">
                             <p>Dont Have Account?<span onClick = {() => navigate('/register')}> Register</span></p>
                         </div>
