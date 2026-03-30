2b78cf7f00d49a9a0eb2effa15a479f0150f9a5b
waile7 <waile.zaraani0000@gmail.com>
2026-03-30 08:46:35 +0100

    page register is updated

---
+++ FrontEnd/src/pages/discover/Discover.jsx
@@ -0,0 +1,11 @@
+import './discover.css'
+
+import React from 'react'
+
+function Discover() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Discover
--- FrontEnd/src/pages/register/Register.jsx
+++ FrontEnd/src/pages/register/Register.jsx
@@ -2,32 +2,43 @@ import './register.css'
 import openEye from "../../assets/svgs/eye-open.svg"
 import closeEye from "../../assets/svgs/eye-close.svg"
 import logoWithName from "../../assets/logo/logo_with_name.svg"
-import defaultProfilePicture from "../../assets/profile_picture/default_profile.png"
-import { useState } from 'react'
+import defaultprofile_picture from "../../assets/profile_picture/default_profile.jpg"
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../../api.js'
 import React from 'react'
 
 function Register() {
+  const navigate = useNavigate()
 
-  const [errors, setErrors] = useState([])
-  const [passwordOpen, setPasswordOpen] = useState(false)
-  const [infoRegister, setInfoRegister] = useState({
-    firstName: '',
-    lastName: '',
+  const [errors, setErrors] = useState({
+    first_name: '',
+    last_name: '',
     email: '',
     password: '',
-    isDj: false,
-    stageName: null,
-    bio: null,
-    confirmationTerms: false,
-    profilePicture: null
+    stage_name: '',
+    bio: '',
+    confirmation_terms: ''
   })
+  const [passwordOpen, setPasswordOpen] = useState(true)
+  const [textareaCount, setTextareaCount] = useState(0)
+  const [infoRegister, setInfoRegister] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    is_dj: false,
+    stage_name: '',
+    bio: '',
+    confirmation_terms: false,
+    profile_picture: null,
+  })
+  const [statePage, setStatePage] = useState('idle')
   const handleConfirmationTerms = () => {
     setInfoRegister((older) => {
       return {
         ...older,
-        confirmationTerms: !infoRegister.confirmationTerms
+        confirmation_terms: !infoRegister.confirmation_terms
       }
     })
   }
@@ -35,7 +46,7 @@ function Register() {
     setInfoRegister((older) => {
       return {
         ...older,
-        isDj: !infoRegister.isDj
+        is_dj: !infoRegister.is_dj
       }
     })
   }
@@ -48,8 +59,9 @@ function Register() {
         [name]: value
       }
     })
+    if (name === 'bio') setTextareaCount(() => value.length)
   }
-  const handleProfilePicture = (e) => {
+  const handleprofile_picture = (e) => {
     const file = e.target.files[0]
     if (file && file.type.startsWith('image/')) {
       const reader = new FileReader()
@@ -57,7 +69,7 @@ function Register() {
         setInfoRegister((infos) => {
           return {
             ...infos,
-            profilePicture : reader.result
+            profile_picture: reader.result
           }
         })
       }
@@ -66,6 +78,66 @@ function Register() {
 
 
   }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatePage('waiting')
+    if (!infoRegister.confirmation_terms) {
+      setStatePage('error');
+      setErrors(prev => ({
+        ...prev,
+        confirmation_terms: 'You must accept the terms and conditions to continue.'
+      }))
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('first_name', infoRegister.first_name);
+    formData.append('last_name', infoRegister.last_name);
+    formData.append('email', infoRegister.email);
+    formData.append('password', infoRegister.password);
+    formData.append('is_dj', infoRegister.is_dj ? 1 : 0);
+    infoRegister.is_dj ? formData.append('stage_name', infoRegister.stage_name) : null;
+    infoRegister.is_dj ? formData.append('bio', infoRegister.bio) : null;
+    formData.append('confirmation_terms', infoRegister.confirmation_terms ? 1 : 0);
+
+    if (infoRegister.profile_picture instanceof File) {
+      formData.append('profile_picture', infoRegister.profile_picture);
+    }
+
+    try {
+      const res = await api.post('/register', formData, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      localStorage.setItem('token', res.data.token)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
+      console.log(res.data)
+      navigate("/discover")
+    } catch (error) {
+      if (error.response?.status === 422) {
+        const backendErrors = error.response.data.errors;
+        setErrors(prev => ({
+          first_name: backendErrors.first_name?.[0] || '',
+          last_name: backendErrors.last_name?.[0] || '',
+          email: backendErrors.email?.[0] || '',
+          password: backendErrors.password?.[0] || '',
+          stage_name: backendErrors.stage_name?.[0] || '',
+          bio: backendErrors.bio?.[0] || '',
+        }));
+        setStatePage('error');
+        console.log(backendErrors);
+        
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log(errors)
+  }, [errors])
 
 
 
@@ -81,67 +153,82 @@ function Register() {
           <div className="container-profile-title">
             <h2 className="title">Create Account</h2>
             <div className="container-profile">
-              <img src={!infoRegister.profilePicture ? defaultProfilePicture : infoRegister.profilePicture} alt="profile_picture" />
-              <input type="file" name="profile_picture" onChange={(e) => handleProfilePicture(e)} />
+              <img src={!infoRegister.profile_picture ? defaultprofile_picture : infoRegister.profile_picture} alt="profile_picture" />
+              <label htmlFor="profile_picture" className='label-profile-picture'>Upload Profile</label>
+              <input type="file" id='profile_picture' name="profile_picture" onChange={(e) => handleprofile_picture(e)} />
             </div>
           </div>
-          <form action="" method="POST" className="register-form">
-            <div className='full-name-section'>
+          <form onSubmit={(e) => handleSubmit(e)} method="POST" className="register-form">
+            <div className='double-inputs-section'>
               <div className="container-input first-name">
                 <label htmlFor="first-name">First Name</label>
-                <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.bio} name='firstName' />
+                <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.first_name} name='first_name' />
+                {errors.first_name ? <p className='error'>{errors.first_name}</p> : undefined}
               </div>
               <div className="container-input last-name">
                 <label htmlFor="last-name">Last Name</label>
-                <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.lastName} name='lastName' />
+                <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.last_name} name='last_name' />
+                {errors.last_name ? <p className='error'>{errors.last_name}</p> : undefined}
               </div>
             </div>
-            <div className="container-input email">
-              <label htmlFor="email">Email</label>
-              <input type="email" onChange={(e) => handleInputs(e)} value={infoRegister.email} name='email' />
+            <div className="double-inputs-section">
+              <div className="container-input email">
+                <label htmlFor="email">Email</label>
+                <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.email} name='email' />
+                {errors.email ? <p className='error'>{errors.email}</p> : undefined}
+              </div>
+              <div className="container-input password">
+                <label htmlFor="password">Password</label>
+                <input type={!passwordOpen ? 'text' : 'password'} onChange={(e) => handleInputs(e)} value={infoRegister.password} name='password' />
+                {errors.password ? <p className='error'>{errors.password}</p> : undefined}
+                <img className='password-eye' src={passwordOpen ? closeEye : openEye} alt="eye" onClick={() => setPasswordOpen(!passwordOpen)} />
+              </div>
             </div>
-            <div className="container-input password">
-              <label htmlFor="password">Password</label>
-              <input type={passwordOpen ? 'text' : 'password'} onChange={(e) => handleInputs(e)} value={infoRegister.password} name='password' />
-              <img src={passwordOpen ? closeEye : openEye} alt="eye" onClick={() => setPasswordOpen(!passwordOpen)} />
-            </div>
+
             <div className="dj-section">
-              <p className="question-dj"><em>T|T</em> Are You Dj?</p>
+              <p className="question-dj"><b>T|T </b> Are You Dj?</p>
               <div className="check-dj">
-                <input type="checkbox" onChange={() => handleDjContent()} checked={infoRegister.isDj ? true : false} name='isDj' />
+                <input type="checkbox" onChange={() => handleDjContent()} checked={infoRegister.is_dj ? true : false} name='is_dj' />
                 <p className="accept-conditions">
                   I want to <span>Upload mixes</span> and <span>host events</span>
                 </p>
               </div>
-              {infoRegister.isDj && (
-                <>
+              {infoRegister.is_dj && (
+                <div className='info-dj'>
                   <div className="container-input stage-name">
                     <label htmlFor="first-name">Stage Name</label>
-                    <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.stageName} name='stageName' />
+                    <input type="text" onChange={(e) => handleInputs(e)} value={infoRegister.stage_name} name='stage_name' />
+                    {errors.stage_name ? <p className='error'>{errors.stage_name}</p> : undefined}
                   </div>
                   <div className="container-input bio">
                     <label htmlFor="first-name">Bio</label>
-                    <textarea name="bio" onChange={(e) => handleInputs(e)} value={infoRegister.bio} id="bio"></textarea>
+                    <p className='count'>{textareaCount}/300</p>
+                    <textarea maxLength="300" name="bio" onChange={(e) => handleInputs(e)} value={infoRegister.bio} id="bio"></textarea>
+                    {errors.bio ? <p className='error'>{errors.bio}</p> : undefined}
                   </div>
-                </>
+                </div>
               )}
 
             </div>
 
-            <button className='sign-up' type="submit">Sign Up</button>
-            <div className="bottom-button-section">
-              <div className="left">
-                <input type="checkbox" checked={infoRegister.confirmationTerms ? true : false} name='confirmation' onChange={() => handleConfirmationTerms()} />
-                <p>I confirm that i own the rights to the content i upload or have permission to share it.</p>
-              </div>
-              <div className="right">
-                <p>Have An account already?<span>Login</span></p>
+            <div className="button-section">
+              <div className={`waiting-icon ${statePage === "waiting" ? "waiting" : undefined}`}></div>
+              <button type="submit" className={`sign-up ${statePage === "waiting" ? "waiting" : undefined}`}>Sign Up</button>
+              <div className="bottom-button-section">
+                <div className="left">
+                  <div className="confiramtion-section">
+                    <input type="checkbox" checked={infoRegister.confirmation_terms ? true : false} name='confirmation' onChange={() => handleConfirmationTerms()} />
+                    <p>I confirm that i own the rights to the content i upload or have permission to share it.</p>
+                  </div>
+                  {errors.confirmation_terms ? <p className='error'>{errors.confirmation_terms}</p> : undefined}
+                </div>
+                <div className="right">
+                  <p>Have An account already?<span onClick={() => navigate('/login')}>Login</span></p>
+                </div>
               </div>
             </div>
           </form>
-          <div className="link-terms">
-            <a href="">Terms & Content Policy</a>
-          </div>
+          <a href="" className='link-terms'>Terms & Content Policy</a>
         </div>
       </div>
     </div>
