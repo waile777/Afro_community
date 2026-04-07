26d7ff6da0c93ffa79b008fb02c4072e3940f106
= <=>
2026-04-07 10:32:46 +0100

    ticketcontroller added

--- BackEnd/app/Models/Event.php
+++ BackEnd/app/Models/Event.php
@@ -50,4 +50,9 @@ class Event extends Model
     {
         return $this->hasMany(Reservation::class);
     }
+
+    public function tickets():HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowController;
@@ -81,4 +82,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/events', [EventController::class, 'store']);
     Route::patch('/events/{id}', [EventController::class, 'update']);
     Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+
+    //tickets
+    Route::post('/tickets' , [TicketController::class , 'store']);
+    Route::delete('/tickets/{id}' , [TicketController::class , 'destroy']);
+    Route::patch('/tickets/{id}' , [TicketController::class , 'update']);
+
 });
